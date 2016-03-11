373f361a427f4bd9805e745aa6b3d3c027c2e6b6
Seto Elkahfi <setoelkahfi@gmail.com>
2016-03-11 14:00:34 +0700

    Create view programmatically

--- iMusic/ViewController.m
+++ iMusic/ViewController.m
@@ -20,4 +20,28 @@
 	[super viewDidUnload];
 }
 
+- (void)loadView {
+    
+    self.view = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.view.backgroundColor= [UIColor whiteColor];
+    
+    UIView *v1 = [[UIView alloc] initWithFrame:CGRectMake(10.0f, 10.0f, 150.0f, 150.0f)];
+    v1.backgroundColor = [UIColor redColor];
+    v1.tag = 10;
+    
+    UIView *v2 = [[UIView alloc] initWithFrame:CGRectMake(20.0f, 20.0f, 150.0f, 150.0f)];
+    v2.backgroundColor = [UIColor greenColor];
+    v2.tag = 20;
+    
+    UIView *v3 = [[UIView alloc] initWithFrame:CGRectMake(30.0f, 30.0f, 150.0f, 150.0f)];
+    v3.backgroundColor = [UIColor blueColor];
+    v3.tag = 30;
+    
+    [self.view addSubview:v1];
+    [self.view addSubview:v2];
+    [self.view addSubview:v3];
+    
+    
+}
+
 @end

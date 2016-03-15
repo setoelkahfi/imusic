103054a67d1fcbd230f724df9e1fe5a79728b9d7
Seto Elkahfi <setoelkahfi@gmail.com>
2016-03-15 10:09:41 +0700

    Add artist and album data models

---
+++ iMusic/Album.h
@@ -0,0 +1,21 @@
+//
+//  Album.h
+//  iMusic
+//
+//  Created by SDMobile.
+//  Copyright (c) 2016 Streetdirectory Pte Ltd. All rights reserved.
+//
+
+#import "Artist.h"
+@interface Album : NSObject
+
+@property (nonatomic, assign) NSUInteger albumID;
+@property (nonatomic, copy) NSString *albumName;
+@property (nonatomic, copy) NSString *imageURLString;
+@property (nonatomic, copy) NSString *price;
+@property (nonatomic, copy) NSString *iTunesURLString;
+@property (nonatomic, strong) NSDate *releaseDate;
+@property (nonatomic, copy) NSString *genre;
+@property (nonatomic, strong) Artist *artist;
+
+@end
---
+++ iMusic/Artist.h
@@ -0,0 +1,17 @@
+//
+//  Artist.h
+//  iMusic
+//
+//  Created by SDMobile.
+//  Copyright (c) 2016 Streetdirectory Pte Ltd. All rights reserved.
+//
+
+@interface Artist : NSObject
+
+@property (nonatomic, assign) NSUInteger artistID;
+@property (nonatomic, copy) NSString *artistName;
+
++ (id)artistWithID:(NSUInteger)artistID name:(NSString *)artistName;
+- (id)initWithID:(NSUInteger)artistID name:(NSString *)artistName;
+
+@end

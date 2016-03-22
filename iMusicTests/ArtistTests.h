58e5a6856dc6165dd630f93741dfaf730f80fb4b
Seto Elkahfi <setoelkahfi@gmail.com>
2016-03-22 10:59:45 +0700

    Using XCTest framework

---
+++ iMusicTests/ArtistTests.h
@@ -0,0 +1,13 @@
+//
+//  ArtistTests.h
+//  iMusic
+//
+//  Created by SDMobile.
+//  Copyright (c) 2016 Streetdirectory Pte Ltd. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ArtistTests : XCTestCase
+
+@end
---
+++ iMusicTests/ArtistTests.m
@@ -0,0 +1,20 @@
+//
+//  ArtistTests.m
+//  iMusic
+//
+//  Created by SDMobile.
+//  Copyright (c) 2016 Streetdirectory Pte Ltd. All rights reserved.
+//
+
+#import "ArtistTests.h"
+
+@implementation ArtistTests
+
+- (void)testInitWithIDName {
+    NSUInteger artistID = 100;
+    NSString *artistName = @"Metallica";
+    
+    
+}
+
+@end

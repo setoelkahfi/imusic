45b487d31a60da08024a07b98e99c3869d5550c5
Seto Elkahfi <setoelkahfi@gmail.com>
2016-03-22 11:07:51 +0700

    testInitWithIDName

--- iMusicTests/ArtistTests.m
+++ iMusicTests/ArtistTests.m
@@ -7,6 +7,7 @@
 //
 
 #import "ArtistTests.h"
+#import "Artist.h"
 
 @implementation ArtistTests
 
@@ -14,7 +15,10 @@
     NSUInteger artistID = 100;
     NSString *artistName = @"Metallica";
     
+    Artist *artist = [[Artist alloc] initWithID:artistID name:artistName];
     
+    XCTAssertEqual(artist.artistID, artistID);
+    XCTAssertEqualObjects(artist.artistName, artistName);
 }
 
 @end

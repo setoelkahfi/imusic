a6b7341b34a6fa39091ea86807933d7022b46f61
Seto Elkahfi <setoelkahfi@gmail.com>
2016-03-28 11:08:00 +0700

    Use storyboard for user interface.

--- iMusic/AppDelegate.m
+++ iMusic/AppDelegate.m
@@ -18,18 +18,6 @@
 @synthesize window = _window;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    
-    // Override point for customization after application launch.
-    
-    self.window.backgroundColor = [UIColor whiteColor];
-
-    id controller = [[MainViewController alloc] initWithNibName:@"MainView" bundle:nil];
-    id navController = [[UINavigationController alloc] initWithRootViewController:controller];
-    self.window.rootViewController = navController;
-    
-    [self.window makeKeyAndVisible];
     return YES;
 }
 
